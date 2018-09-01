ac62cc30583c4be044c74aa3d94850b9c3c977e3
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-09-01 10:40:01 +0700

    Add `o2q` Variant

---
+++ o2q.js
@@ -0,0 +1,69 @@
+/*! <https://github.com/tovic/query-string-parser> */
+(function(win, NS) {
+
+    function object_serialize(x, separator, deep, include_false) {
+
+        function encode(x) {
+            return encodeURIComponent(x);
+        }
+
+        function is_set(x) {
+            return typeof x !== "undefined";
+        }
+
+        function is_string(x) {
+            return typeof x === "string";
+        }
+
+        function is_object(x) {
+            return x !== null && typeof x === "object";
+        }
+
+        function str(x) {
+            if (x === true) return 'true';
+            if (x === false) return 'false';
+            if (x === null) return 'null';
+            if (is_object(x)) return JSON.stringify(x);
+            return x + "";
+        }
+
+        function q(arr, key, depth, deep) {
+            depth = depth || 0;
+            var out = [],
+                s = key ? '%5D' : "", i, k, v;
+            for (i in arr) {
+                k = encode(i);
+                v = arr[i];
+                if (is_object(v)) {
+                    if (depth < deep) {
+                        Object.assign(out, q(v, key + k + s + '%5B', depth + 1, deep));
+                    } else {
+                        out[key + k + s] = v;
+                    }
+                } else {
+                    out[key + k + s] = v;
+                }
+            }
+            return out;
+        }
+
+        deep = deep || 1;
+
+        var out = [],
+            arr = q(x, "", 0, deep),
+            k, v;
+
+        for (k in arr) {
+            v = arr[k];
+            if (v === false && !include_false) continue; // `['a' => 'false', 'b' => false]` → `a=false`
+            v = v !== true ? '=' + encode(str(v)) : ""; // `['a' => 'true', 'b' => true]` → `a=true&b`
+            out.push(k + v); // `['a' => 'null', 'b' => null]` → `a=null&b=null`
+        }
+
+        return out.length ? '?' + out.join(separator || '&') : "";
+
+    }
+
+    win[NS] = object_serialize;
+
+})(window, 'o2q');
--- q2o.js
+++ q2o.js
@@ -1,76 +1,79 @@
-// <https://github.com/tovic/query-string-parser>
+/*! <https://github.com/tovic/query-string-parser> */
 (function(win, NS) {
 
-    function decode(x) {
-        return decodeURIComponent(x);
-    }
-
-    function is_set(x) {
-        return typeof x !== "undefined";
-    }
-
-    function is_string(x) {
-        return typeof x === "string";
-    }
-
-    function is_object(x) {
-        return typeof x === "object";
-    }
-
-    function maybe_json(x) {
-        if (!is_string(x) || x.trim() === "") {
-            return false;
-        }
-        return (
-            // Maybe an empty string, array or object
-            x === '""' ||
-            x === '[]' ||
-            x === '{}' ||
-            // Maybe an encoded JSON string
-            x[0] === '"' && x.slice(-1) === '"' ||
-            // Maybe a numeric array
-            x[0] === '[' && x.slice(-1) === ']' ||
-            // Maybe an associative array
-            x[0] === '{' && x.slice(-1) === '}'
-        );
-    }
-
-    function str_eval(x) {
-        if (is_string(x)) {
-            if (x === 'true') {
-                return true;
-            } else if (x === 'false') {
-                return false;
-            } else if (x === 'null') {
-                return null;
-            } else if (x.slice(0, 1) === "'" && x.slice(-1) === "'") {
-                return x.slice(1, -1);
-            } else if (/^-?(\d*\.)?\d+$/.test(x) && x >= Number.MIN_SAFE_INTEGER && x <= Number.MAX_SAFE_INTEGER) {
-                return +x;
-            } else if (maybe_json(x)) {
-                try {
-                    return JSON.parse(x);
-                } catch (e) {}
-            }
-        }
-        return x;
-    }
-
-    function query(o, props, value) {
-        var path = props.split('['), k;
-        for (var i = 0, j = path.length; i < j - 1; ++i) {
-            k = path[i].replace(/\]$/, "");
-            o = o[k] || (o[k] = {});
-        }
-        o[path[i].replace(/\]$/, "")] = value;
-    }
-
     function query_eval(x, eval) {
+
+        function decode(x) {
+            return decodeURIComponent(x);
+        }
+
+        function is_set(x) {
+            return typeof x !== "undefined";
+        }
+
+        function is_string(x) {
+            return typeof x === "string";
+        }
+
+        function is_object(x) {
+            return x !== null && typeof x === "object";
+        }
+
+        function maybe_json(x) {
+            if (!is_string(x) || x.trim() === "") {
+                return false;
+            }
+            return (
+                // Maybe an empty string, array or object
+                x === '""' ||
+                x === '[]' ||
+                x === '{}' ||
+                // Maybe an encoded JSON string
+                x[0] === '"' && x.slice(-1) === '"' ||
+                // Maybe a numeric array
+                x[0] === '[' && x.slice(-1) === ']' ||
+                // Maybe an associative array
+                x[0] === '{' && x.slice(-1) === '}'
+            );
+        }
+
+        function str_eval(x) {
+            if (is_string(x)) {
+                if (x === 'true') {
+                    return true;
+                } else if (x === 'false') {
+                    return false;
+                } else if (x === 'null') {
+                    return null;
+                } else if (x.slice(0, 1) === "'" && x.slice(-1) === "'") {
+                    return x.slice(1, -1);
+                } else if (/^-?(\d*\.)?\d+$/.test(x) && x >= Number.MIN_SAFE_INTEGER && x <= Number.MAX_SAFE_INTEGER) {
+                    return +x;
+                } else if (maybe_json(x)) {
+                    try {
+                        return JSON.parse(x);
+                    } catch (e) {}
+                }
+            }
+            return x;
+        }
+
+        function query(o, props, value) {
+            var path = props.split('['), k;
+            for (var i = 0, j = path.length; i < j - 1; ++i) {
+                k = path[i].replace(/\]$/, "");
+                o = o[k] || (o[k] = {});
+            }
+            o[path[i].replace(/\]$/, "")] = value;
+        }
+
         var out = {},
             part = x.replace(/^.*?\?/, "");
+
         if (part === "") {
             return out;
         }
+
         part.split(/&(?:amp;)?/).forEach(function(v) {
             var a = v.split('='),
                 key = decode(a[0]),
@@ -84,7 +87,9 @@
                 out[key] = value;
             }
         });
+
         return out;
+
     }
 
     win[NS] = query_eval;
